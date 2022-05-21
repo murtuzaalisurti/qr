f1e8bde0c1c742c897e1820234efa7828def1273
murtuza <murtuza.creativity@gmail.com>
2022-05-21 13:54:22 +0530

    application ready ✔

--- public/js/scanner.js
+++ public/js/scanner.js
@@ -4,12 +4,17 @@ let scanner = new Html5Qrcode("reader");
 function scan(){
     // This method will trigger user permissions
     Html5Qrcode.getCameras().then(devices => {
+
+        document.querySelector(".scan-btn").style = "display: none;";
+        document.querySelector(".scan-btn").innerHTML = "Allow access to camera";
+
+        document.querySelector("#camera").style = "";
+        document.querySelector(".scan").style = "";
         /**
          * devices would be an array of objects of type:
          * { id: "id", label: "label" }
          */
          if (devices && devices.length) {
-            console.log(devices);
 
             let select = document.querySelector("#camera");
 
@@ -23,33 +28,51 @@ function scan(){
             document.querySelector('.scan').addEventListener('click', () => {
                 
                 cameraId = select.options[select.selectedIndex].value;
-                console.log(cameraId);
                 // .. use this to start scanning.
         
-                scanner.start(
-                    cameraId,
-                    {
-                        fps: 10,
-                        qrbox: {
-                            width: 250,
-                            height: 250
+                if(cameraId !== "") {
+
+                    document.querySelector("#camera").style = "display: none;";
+
+                    scanner.start(
+                        cameraId,
+                        {
+                            fps: 24,
+                            qrbox: {
+                                width: 300,
+                                height: 300
+                            }
+                        },
+                        (decodedText, decodedResult) => {
+                            
+                            document.querySelector(".res").innerHTML = `${decodedText}`;
+                            document.querySelector(".res").style = "";
+                            console.log(`Code matched = ${decodedResult}`);
+                            scanner.pause(true);
+                        },
+                        (errorMessage) => {
+                            // console.error(`Code scan error = ${errorMessage}`);
                         }
-                    },
-                    (decodedText, decodedResult) => {
-                        document.querySelector(".res").innerHTML = `Code matched = ${decodedText} ${decodedResult}`;
-                        console.log(`Code matched = ${decodedText}`, decodedResult);
-                    },
-                    (errorMessage) => {
-                        // console.error(`Code scan error = ${errorMessage}`);
-                    }
-                ).catch((err) => {
-                    console.error(err);
-                })
+                    ).then(() => {
+                        console.log('camera started')
+                        document.querySelector('.scan').disabled = true;
+                        document.querySelector(".stop-btn").disabled = false;
+                        document.querySelector(".stop-btn").style = "";
+                    }).catch((err) => {
+                        console.error(err);
+                    })
+                } else {
+                    document.querySelector("#camera").focus();
+                }
             })
         }
         
     }).catch(err => {
         // handle err
+        console.error(err);
+        document.querySelector(".scan-btn").style = "";
+        document.querySelector(".scan-btn").disabled = true;
+        document.querySelector(".error-msg").style = "";
     });
 
 }
@@ -66,6 +89,11 @@ function stopScan() {
     scanner.stop().then((ignore) => {
         // QR Code scanning is stopped.
         console.log('Scan stopped')
+        document.querySelector(".res").style = "display: none;";
+        document.querySelector('.scan').disabled = false;
+        document.querySelector('.stop-btn').disabled = true;
+        document.querySelector('.stop-btn').style = "display: none";
+        document.querySelector("#camera").style = "";
       }).catch((err) => {
         // Stop failed, handle it.
         console.log(err);
