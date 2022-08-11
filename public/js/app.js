d18f4b2f6d0cf43b6ada394c594603aa77ea5076
murtuza <murtuza.creativity@gmail.com>
2022-08-11 11:41:12 +0530

    added prod js ✔

--- public/js/app.js
+++ public/js/app.js
@@ -68,11 +68,11 @@ document.querySelectorAll(".select-section button").forEach((ele) => {
     })
 })
 
-if ("serviceWorker" in navigator) {
+/*if ("serviceWorker" in navigator) {
     window.addEventListener("load", function () {
         navigator.serviceWorker
             .register("/serviceWorker.js")
             .then(res => console.log("service worker registered"))
             .catch(err => console.log("service worker not registered", err))
     })
-}
+}*/
