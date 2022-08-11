433cd4fe4ac89109e4779b34fb789f30474b08c1
murtuza <murtuza.creativity@gmail.com>
2022-08-11 12:11:06 +0530

    testing

--- public/js/app.js
+++ public/js/app.js
@@ -83,11 +83,11 @@ document.querySelectorAll(".select-section button").forEach((ele) => {
     });
 });
 
-if ("serviceWorker" in navigator) {
-    window.addEventListener("load", function () {
-        navigator.serviceWorker
-            .register("/serviceWorker.js")
-            .then((res) => console.log("service worker registered"))
-            .catch((err) => console.log("service worker not registered", err));
-    });
-}
+// if ("serviceWorker" in navigator) {
+//     window.addEventListener("load", function () {
+//         navigator.serviceWorker
+//             .register("/serviceWorker.js")
+//             .then((res) => console.log("service worker registered"))
+//             .catch((err) => console.log("service worker not registered", err));
+//     });
+// }
